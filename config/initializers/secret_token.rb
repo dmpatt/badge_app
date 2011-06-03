01b1d3c31d8b68ebed70fac1d1df16ef659db6e5
David Patt <david.m.patt@gmail.com>
2011-06-02 20:50:05 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BadgeApp::Application.config.secret_token = '26672a74ebfdd50409eee9ef1c4540ec95c19f4747b9b47a27a9f6105dfed9ecc819654a5e24c89fcb35edcedb1ffa1e3acf0b2099a8cf77482ba9b0ffdf3fcc'
